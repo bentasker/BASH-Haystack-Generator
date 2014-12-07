01f556208a8cfd29ad8361ee8a76651f9293d08f
B Tasker <github@bentasker.co.uk>
2014-12-07 16:58:31 +0000

    Added ability to also send random data upstream

--- client-side/request_generator.sh
+++ client-side/request_generator.sh
@@ -16,10 +16,16 @@ placeRequest(){
 	HOST=$2
 	START=$3
 	END=$4
+	UPSTREAM=$5
+	DATA=$6
 
-
-	# Place the request
-	curl -H "Host: $HOST" -H "Range: bytes=$START-$END" "$URL" > /dev/null 2> /dev/null
+	if [ "$UPSTREAM" == 'n' ]
+	then
+		# Place the request
+		curl -H "Host: $HOST" -H "Range: bytes=$START-$END" "$URL" > /dev/null 2> /dev/null
+	else
+		curl -X POST -d "$DATA" -H "Host: $HOST" -H "Range: bytes=$START-$END" "$URL" > /dev/null 2> /dev/null
+	fi
 }
 
 
@@ -38,6 +44,13 @@ then
 fi
 
 
+if [[ "$SEND_DATA" == "y" ]] && [[ "$RANDOMIZE_US_CHUNKSIZE" == "n" ]]
+then
+	UPSTREAM_LEN=$((RANDOM%$MAX_UPSTREAM))
+	UPSTREAM_DATA=`cat /dev/urandom | tr -dc 'a-zA-Z0-9-_@#$%^&*()_+{}|:<>?='|fold -w $UPSTREAM_LEN| head -n 1`
+fi
+
+
 echo "Will place $REQUESTS Request(s)"
 
 for i in `seq 1 $REQUESTS`
@@ -47,14 +60,29 @@ do
 		CHUNKS=$((RANDOM%$MAX_CHUNK_SIZE)) # Chunk size
 	fi
 
+	if [[ "$SEND_DATA" == "y" ]] && [[ "$RANDOMIZE_US_CHUNKSIZE" == "y" ]]
+	then
+		UPSTREAM_LEN=$((RANDOM%$MAX_UPSTREAM))
+		UPSTREAM_DATA=`cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?='|fold -w $UPSTREAM_LEN| head -n 1`
+	fi
+
+
+
 	START=$((RANDOM%134217727)) # (1Gib-1byte) - if the end range falls outside 1Gib we'll just be served the remaining 1 byte
 	END=$(( $START + $CHUNKS)) # Calculate the end of the range
 
 	echo "Requesting range $START-$END (Chunk size $CHUNKS bytes) from $REMOTE_FILE"
-	placeRequest "$REMOTE_FILE" "$HTTP_HOSTNAME" "$START" "$END"
+
+	if [[ "$SEND_DATA" == "y" ]]
+	then
+		echo "Sending $UPSTREAM_LEN bytes of meaningless data with the request"
+	fi
+
+	placeRequest "$REMOTE_FILE" "$HTTP_HOSTNAME" "$START" "$END" "$SEND_DATA" "$UPSTREAM_DATA"
 
 	DELAY=$((RANDOM%5)) # Work out how long to delay the next request by
 	echo "Waiting $DELAY seconds until next request"
+	echo 
 	sleep $DELAY
 
 done
